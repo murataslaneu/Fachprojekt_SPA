d3830b1a5195c3c367a1e7e8eaea7ce6e76a6911
Murat <murat.aslan@tu-dortmund.de>
2025-06-14 16:03:00 +0200

    Add build.sbt and doc comments to RemovedCall and AnalysisResult

--- ex4/src/main/scala/modify/data/AnalysisResult.scala
+++ ex4/src/main/scala/modify/data/AnalysisResult.scala
@@ -3,6 +3,16 @@ package modify.data
 import play.api.libs.json._
 import modify.data.RemovedCall
 
+/**
+ * Represents the result of analyzing and modifying a specific method.
+ *
+ * @param className        Fully qualified name of the class containing the method.
+ * @param methodName       Name of the method that was analyzed.
+ * @param removedCalls     List of critical method calls that were removed.
+ * @param status           Status message about what was done (e.g., whether class was modified).
+ * @param ignored          True if any critical calls were ignored due to ignore list.
+ * @param bytecodeVerified True if the bytecode was successfully verified after modification.
+ */
 case class AnalysisResult(
                            className: String,
                            methodName: String,
@@ -13,5 +23,6 @@ case class AnalysisResult(
                          )
 
 object AnalysisResult {
+  // Enables automatic JSON serialization/deserialization for AnalysisResult
   implicit val format: OFormat[AnalysisResult] = Json.format[AnalysisResult]
 }
